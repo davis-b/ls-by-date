69432ad8957aa2e0d6e7017255699a0b25e6b6de
davis <52375250+davis-b@users.noreply.github.com>
2020-10-02 12:44:56 +0000

    Add required structs

--- main.zig
+++ main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const os = std.os;
 
+const Args = struct {
+    dir: []const u8 = "./",
+    reverse: bool = false,
+    recursive: bool = false,
+};
+
+const File = struct {
+    time: isize,
+    path: []const u8,
+    kind: std.fs.Dir.Entry.Kind,
+};
+
 pub fn main() !void {
-  //
+    //
 }
